9a13900e18864996523a7947d8bd0e105f81ab5f
abduls060 <abduls060@gmail.com>
2019-07-24 04:05:54 +0530

    Python Script Testing- RPi

--- Codes/STM32F407VGT6/STM32F407VGT6/Src/main.c
+++ Codes/STM32F407VGT6/STM32F407VGT6/Src/main.c
@@ -296,7 +296,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOC_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(MEMS_CS_GPIO_Port, MEMS_CS_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(MEMS_CS_GPIO_Port, MEMS_CS_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOD, LED1_Pin|LED2_Pin|LED3_Pin|LED4_Pin, GPIO_PIN_RESET);
