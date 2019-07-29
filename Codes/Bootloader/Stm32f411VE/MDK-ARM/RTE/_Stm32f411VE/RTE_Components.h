02e2775de5c4766a96a1055429d4030f3d52cbbd
jagannath.sahoo <jagannath.sahoo@live.com>
2019-07-29 18:28:18 +0530

    STM32F411VE BOOTLOADER

---
+++ Codes/Bootloader/Stm32f411VE/MDK-ARM/RTE/_Stm32f411VE/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'Stm32f411VE' 
+ * Target:  'Stm32f411VE' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
